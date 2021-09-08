012cadb303666fd50376ea44aa3a7729f5013ecf
rosstimo <rosstimo@isu.edu>
2021-09-08 08:39:09 -0600

    add try catch, convert to int and repeat if not valid number

--- ProgramFlowExamples/ProgramFlowExamples.vb
+++ ProgramFlowExamples/ProgramFlowExamples.vb
@@ -1,6 +1,32 @@
-﻿Module ProgramFlowExamples
+﻿Option Strict On
+Option Explicit On
+
+Module ProgramFlowExamples
 
     Sub Main()
+        Dim userResponse As String
+        Dim userNumber As Integer
+        Dim secondNumber As Integer = 5
+        Dim result As Integer
+        Dim isValidNumber As Boolean = True
+
+        Do
+            Console.WriteLine("Please enter a whole number or Q to quit")
+            userResponse = Console.ReadLine()
+            Console.WriteLine($"You entered {userResponse}")
+            Try
+                userNumber = CInt(userResponse)
+                isValidNumber = True
+            Catch
+                isValidNumber = False
+            End Try
+        Loop While isValidNumber = False
+
+        result = userNumber + secondNumber
+
+        Console.WriteLine($"Result: {userNumber} + {secondNumber} = {result}")
+
+        Console.Read()
 
     End Sub
 
