b9985ba6398c830f99afd71535f2407eff50c2b1
rosstimo <rosstimo@isu.edu>
2021-09-09 08:22:11 -0600

    Working Q to quit

--- ProgramFlowExamples/ProgramFlowExamples.vb
+++ ProgramFlowExamples/ProgramFlowExamples.vb
@@ -1,5 +1,6 @@
 ﻿Option Strict On
 Option Explicit On
+Option Compare Text
 
 Module ProgramFlowExamples
 
@@ -11,32 +12,22 @@ Module ProgramFlowExamples
         Dim isValidNumber As Boolean
 
         Do
+            isValidNumber = True
             Console.WriteLine("Please enter a whole number or Q to quit")
             userResponse = Console.ReadLine()
             Console.WriteLine($"You entered {userResponse}")
-
-            'If userResponse = "Q" Then
-            '    MsgBox("Quitter!")
-            'End If
-
             Try
                 userNumber = CInt(userResponse)
-                isValidNumber = True
+                result = userNumber + secondNumber
+                Console.WriteLine($"Result: {userNumber} + {secondNumber} = {result}")
             Catch
-                isValidNumber = False
+                If userResponse <> "Q" Then
+                    isValidNumber = False
+                End If
             End Try
-
-        Loop While isValidNumber = False And (userResponse <> "Q" Or userResponse <> "q")
-
-        If userResponse <> "Q" Or userResponse <> "q" Then
-            result = userNumber + secondNumber
-            Console.WriteLine($"Result: {userNumber} + {secondNumber} = {result}")
-        Else
-            Console.WriteLine("Have a nice day!!!")
-        End If
-
+        Loop Until isValidNumber = True And (userResponse <> "Q" Or userResponse <> "q")
+        Console.WriteLine("Have a nice day!!!")
         Console.Read()
-
     End Sub
 
 End Module
